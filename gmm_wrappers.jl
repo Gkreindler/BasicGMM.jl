e500be3c796f0ee117d09c2fa96004008d670b6e
Gabriel E Kreindler <Gek@mit.edu>
2022-05-07 21:21:19 -0400

    function to draw random initial conditions

--- gmm_wrappers.jl
+++ gmm_wrappers.jl
@@ -5,6 +5,37 @@ using LsqFit # modified version that accepts time limit. Use https://github.com/
 
 
 """
+Creates a random matrix of initial conditions, taking boudns into account
+"""
+function random_initial_conditions(theta0, theta_lower, theta_upper, n_init)
+
+    n_params = length(theta0)
+
+    theta0_mat = zeros(n_init, n_params)
+
+    for i=1:n_init
+        for j=1:n_params
+
+            θL = theta_lower[j]
+            θH = theta_upper[j]
+
+            if (θL == -Inf) & (θH == Inf)
+                theta0_mat[i,j] = (-1.5 + 3.0 * rand()) * theta0[j]
+            elseif (θL > -Inf) & (θH == Inf)
+                theta0_mat[i,j] = θL + (theta0[j] - θL) * (0.5 + rand())
+            elseif (θL == -Inf) & (θH < Inf)
+                theta0_mat[i,j] = θH - (θH - theta0[j]) * (0.5 + rand())
+            else
+                @assert (θL > -Inf) & (θH < Inf)
+                theta0_mat[i,j] = θL + (θH - θL) * rand()
+            end
+        end
+    end
+
+    return theta0_mat
+end
+
+"""
     theta = typically the first stage estimate
     momfn = moment function loaded with data and other parameters
 """
@@ -182,10 +213,7 @@ function gmm_2step(;
     end
 
 ## Initial weighting matrix W
-	if ~isnothing(Wstep1)
-        show_progress && println("GMM => using provided W1")
-        
-    else
+	if isnothing(Wstep1)
         show_progress && println("GMM => using identity W1")
 
         # if not provided, use identity weighting matrix
